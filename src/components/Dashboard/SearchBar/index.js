754b04ca023bda1abea6808b7c93d1e0ccb6bd5a
belleomoraes <iolliveira.moraes@gmail.com>
2023-02-03 14:28:25 -0300

    feat: handle form changes on search bar

--- src/components/Dashboard/SearchBar/index.js
+++ src/components/Dashboard/SearchBar/index.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
 import { IoSearchCircle } from 'react-icons/io5';
+import Input from '../../Forms/Input';
+import { useState, useEffect } from 'react';
 
 export default function SearchBar() {
+  const [search, setSearch] = useState('');
+
+  function handleChange(e) {
+    e.preventDefault();
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  }
+
   return (
     <Display>
-      <InputBar></InputBar>
-      <IoSearchCircle />
+      <InputBar name="search" value={search.name} onChange={handleChange}></InputBar>
+      <IoSearchCircle cursor="pointer" />
     </Display>
   );
 }
@@ -17,7 +29,7 @@ const Display = styled.div`
   justify-content: space-between;
   font-size: 7vh;
 `;
-const InputBar = styled.div`
+const InputBar = styled.input`
   width: 100%;
   height: 6vh;
   border-width: 2px;
